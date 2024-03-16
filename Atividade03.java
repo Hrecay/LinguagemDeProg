5d14b3e39dda6a036338ffae37e22c2b8975f5f5
Hrecay <130329286+Hrecay@users.noreply.github.com>
2024-03-15 21:44:06 -0300

    Create Atividade03.java

---
+++ Atividade03.java
@@ -0,0 +1,14 @@
+package Aula02;
+
+public class Atividade03 {
+	public static void main(String[] args) {
+		String nome = "Lucas";
+		String CPF = "09215729992";
+		String CEP = "81450620";
+		String Cidade = "Curitiba";
+		String Estado = "PR";
+		
+		System.out.println("A Pessoa com o nome " + nome + " registrada no CPF: " + CPF + ", é residente do CEP:" + CEP + "na cidade de " + Cidade + " - " + Estado);
+		
+	}
+}
